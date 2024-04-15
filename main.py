f1e44d662422934698686050e5cbd4ba932928c9
Advait <apote2050@gmail.com>
2024-04-16 01:44:40 +0530

    merkle root validating

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def merkle_root(txids):
     if len(txids) == 1:
         return txids[0]
         # return bytes.fromhex(txids[0])[::-1].hex()
-    # txids = [bytes.fromhex(txid)[::-1].hex() for txid in txids]
+    txids = [bytes.fromhex(txid)[::-1].hex() for txid in txids]
     while True:
         new_txids = []
         if len(txids) == 1:
@@ -34,12 +34,12 @@ def merkle_root(txids):
         for i in range(0, len(txids), 2):
             new_txids.append(sha256(sha256(bytes.fromhex(txids[i]+txids[i+1])).digest()).digest().hex())
         txids = new_txids
-    # return bytes.fromhex(txids[0])[::-1].hex()
-    return txids[0]
+    return bytes.fromhex(txids[0])[::-1].hex()
+    # return txids[0]
 
 def construct_block_header(txids):
     target = "0000ffff00000000000000000000000000000000000000000000000000000000"
-    merkle = bytes.fromhex(merkle_root(txids)).hex()
+    merkle = bytes.fromhex(merkle_root(txids))[::-1].hex()
     unix_time = bytes.fromhex(hex(int(time.time()))[2:])[::-1].hex()
     target_bits = bytes.fromhex("1f00ffff")[::-1].hex()
     header_pre_nonce = "00c0302f" + "0"*64 + merkle + unix_time + target_bits
@@ -257,10 +257,10 @@ for txname in transaction_fees:
 # print(len(block_arr))
 # print(len(transaction_fees))
 
-print(block_arr[1:3])
-print(merkle_root(block_arr[1:3]))
-print(construct_block_header(block_arr[1:3]))
-block_header = construct_block_header(block_arr[1:3])
+print(block_arr[1:2])
+print(merkle_root(block_arr[1:2]))
+print(construct_block_header(block_arr[1:2]))
+block_header = construct_block_header(block_arr[1:2])
 block_arr = [block_header] + block_arr
 # print(block_arr[0])
 # print(block_arr[1])
@@ -269,8 +269,10 @@ block_arr = [block_header] + block_arr
 print(len(block_arr))
     
 try:
+    # Open the file in write mode
     with open("output.txt", 'w') as file:
-        for element in block_arr[:4]:
+        # Write each element of the list to the file
+        for element in block_arr[:3]:
             file.write(element + '\n') 
         # file.write("00c0302f0000000000000000000000000000000000000000000000000000000000000000654219c9e5445b309a1ef5f0472f5b7ba8bfde00d810dfb89286ba5089a7d8e8377f1d66ffff001f00000AA6" + "\n")
         # file.write("01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff2503d3ce0c184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff01d1af4327000000004341047eda6bd04fb27cab6e7c28c99b94977f073e912f25d1ff7165d9c95cd9bbe6da7e7ad7f2acb09e0ced91705f7616af53bee51a238b7dc527f2be0aa60469d140ac00000000")
