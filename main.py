fb155524bc6cc39084371dce9d758880d14de7cb
Advait <apote2050@gmail.com>
2024-04-15 19:36:38 +0530

    merkle root validating

--- main.py
+++ main.py
@@ -255,22 +255,22 @@ for txname in transaction_fees:
 # print(len(block_arr))
 # print(len(transaction_fees))
 
-print(block_arr[1:3])
-print(merkle_root(block_arr[1:3]))
-print(construct_block_header(block_arr[1:3]))
-block_header = construct_block_header(block_arr[1:2])
+# print(block_arr[1:3])
+# print(merkle_root(block_arr[1:3]))
+# print(construct_block_header(block_arr[1:3]))
+block_header = construct_block_header(block_arr[1:])
 block_arr = [block_header] + block_arr
-print(block_arr[0])
-print(block_arr[1])
-print(block_arr[2])
-print(block_arr[3])
-print(len(block_arr))
+# print(block_arr[0])
+# print(block_arr[1])
+# print(block_arr[2])
+# print(block_arr[3])
+# print(len(block_arr))
     
 try:
     # Open the file in write mode
     with open("output.txt", 'w') as file:
         # Write each element of the list to the file
-        for element in block_arr[:3]:
+        for element in block_arr:
             file.write(element + '\n') 
 except Exception as e:
     print(f"Error writing to file: {e}")
