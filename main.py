0ec325bd631232c4762f33da1f24367f86ddfa44
Advait <apote2050@gmail.com>
2024-04-20 14:08:42 +0530

    testing witness commmitment

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ for file_name in valid_transactions_new:
     # file_name = valid_transactions[i]
     with open('mempool/' + file_name, 'r') as file:
         try:
-            if block_weight > 3000000:
+            if block_weight > 3999000:
                 break
             data = json.load(file)
             txid = sha256(sha256(bytes.fromhex(serialize(data)[1])).digest()).digest().hex()
